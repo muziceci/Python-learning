0e221795df40a63cf4295e3956e3157f4c03646d
muziceci <240970661@qq.com>
2020-08-04 10:37:54 +0800

    commit

--- third/Joseph.py
+++ third/Joseph.py
@@ -1,24 +1,27 @@
 # 约瑟夫环,加上人的属性
 class Person:
-    def __init__(self,name,age,sex):
+    def __init__(self, name, age, sex):
         self.name = name
         self.age = age
         self.sex = sex
+
     def getName(self):
         return self.name
+
     def getAge(self):
         return self.age
+
     def getSex(self):
         return self.sex
         
 kill_lst = []
  
-def get_survive_number(number,step):
-    joseph_lst = [i for i in range(1,total_number+1)]
+def get_survive_number(number, step):
+    joseph_lst = [i for i in range(1, total_number+1)]
     print("约瑟夫环编号是：",joseph_lst)
     temp = 1  #计数
     index = 0 #元素下标
-    while len(joseph_lst)>1:
+    while len(joseph_lst) > 1:
         if temp == step:
             kill_lst.append(joseph_lst[index])
             joseph_lst.pop(index)
@@ -30,15 +33,16 @@ def get_survive_number(number,step):
             index = 0
     return joseph_lst[0]
 
+
 total_number = int(input("请输入总人数:"))
 kill_number = int(input("请输入会被杀掉的报数:"))
 
-p1 = Person('Jack',19,'male')
-p2 = Person('Vivi',17,'female')
-p3 = Person('Mark',21,'male')
-person_lst = [p1,p2,p3]
-result = get_survive_number(total_number,kill_number)
+p1 = Person('Jack', 19, 'male')
+p2 = Person('Vivi', 17, 'female')
+p3 = Person('Mark', 21, 'male')
+person_lst = [p1, p2, p3]
+result = get_survive_number(total_number, kill_number)
 assert(result == 3) #如果预期结果不是3则程序中断，报错
-print("最后留下的人的属性:",end=" ")
-print("姓名：",person_lst[result-1].getName(),"年龄：",person_lst[result-1].getAge(),"性别：",person_lst[result-1].getSex())
-print("被杀掉的编号:",kill_lst)
+print("最后留下的人的属性:", end=" ")
+print("姓名：", person_lst[result-1].getName(),"年龄：",person_lst[result-1].getAge(), "性别：", person_lst[result-1].getSex())
+print("被杀掉的编号:", kill_lst)
