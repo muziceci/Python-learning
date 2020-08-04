97d59847d001e60f37710b30c4517f8abb9e0a2a
muziceci <240970661@qq.com>
2020-08-04 10:41:41 +0800

    delete

--- practice1/1.py
+++
@@ -1,6 +0,0 @@
-a=[1,2,3,4,5,6,7,8,9,0]
-print("The original list:",a)
-a.pop(0)
-
-a.append(1)
-print("The new list:",a)
--- practice1/3.py
+++
@@ -1,7 +0,0 @@
-a="How  are  you"
-print(a)
-
-b=a.split("  ")
-print(b)
-
-print(' '.join(b))
